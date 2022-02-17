8deec5d1bc5443ce4f655b6682db39d1c5bcf47a
Andrei0205 <andyr3.ae@gmail.com>
2022-02-17 21:18:55 +0200

    Add files via upload

---
+++ Flash Chat iOS13.xcodeproj/Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Andrei Marinescu on 30.03.2021.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
